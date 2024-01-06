0b3f589942a1ba4c49890a0ed83eb859c95fbcb3
Alexandru Geana <ab@geana.io>
2024-01-06 20:06:20 +0100

    work through adc_control.rs

--- firmware/src/adc_control.rs
+++ firmware/src/adc_control.rs
@@ -7,19 +7,35 @@ use pid::Pid;
 
 pub struct ADCControl {
     pub enabled: bool,
-    // Time interval between two controls (in ticks: 1 second corresponds to 8047640 ticks).
+    /// Time interval between two controls in systimer ticks.
     pub control_ticks: u64,
     pub last_control: u64,
     pub pid: Pid<f32>,
 }
 
 impl ADCControl {
+    /// Magic value in flash to indicate that it contains data.
+    const DATA_MAGIC: u32 = 0x444E4A4E;
+    /// The sector where the data will be serialized.
+    const FLASH_SECTOR: u8 = 3;
+    /// The amount of time between control updates.
+    const CONTROL_DELTA: u64 = 1000; // ~ 1ms
+
     pub fn new() -> ADCControl {
         let mut adc = ADCControl {
             enabled: true,
-            control_ticks: SystemTimer::FREQ / 1000, // ~1milliseconds
+            control_ticks: SystemTimer::FREQ / Self::CONTROL_DELTA,
             last_control: 0,
-            pid: Pid::new(100.0, 0.0, 0.0, 200.0, 200.0, 200.0, 200.0, 0.0),
+            pid: Pid::new(
+                100.0, /* kp */
+                0.0,   /* ki */
+                0.0,   /* kd */
+                200.0, /* p_limit */
+                200.0, /* i_limit */
+                200.0, /* d_limit */
+                200.0, /* output_limit */
+                0.0,   /* setpoint */
+            ),
         };
         adc.read_from_flash();
         adc
@@ -37,9 +53,10 @@ impl ADCControl {
         return (output + self.pid.output_limit) as u16;
     }
 
+    /// Returns true if
+    ///     control is enabled
+    ///     the delta between `ticks` and the last update is more than 1ms
     pub fn needs_control(&self, ticks: u64) -> bool {
-        // The control must be enabled and
-        // the last control has been executed long time enough
         self.enabled && (ticks.abs_diff(self.last_control) > self.control_ticks)
     }
 
@@ -54,18 +71,13 @@ impl ADCControl {
         self.pid.reset_integral_term();
     }
 
-    // Magic value in flash to indicate that it contains data.
-    const IS_SET: u32 = 0x444E4A4E;
-    // The sector where the data will be serialized.
-    const FLASH_SECTOR: u8 = 3;
-
     pub fn read_from_flash(&mut self) {
         let address: *const u32 = Flash::base_address_for_sector(Self::FLASH_SECTOR);
         unsafe {
-            if address.read() != Self::IS_SET {
-                // The memory does not contain the expected data.
+            if address.read() != Self::DATA_MAGIC {
                 return;
             }
+
             self.pid.kp = f32::from_bits(address.offset(1).read());
             self.pid.ki = f32::from_bits(address.offset(2).read());
             self.pid.kd = f32::from_bits(address.offset(3).read());
@@ -77,15 +89,18 @@ impl ADCControl {
 
     pub fn store_in_flash(&self, flash: &Flash) {
         flash.flash_erase_sector(Self::FLASH_SECTOR);
+
         let base: *mut u32 = Flash::base_address_for_sector(Self::FLASH_SECTOR);
+
         let values = [
-            Self::IS_SET, // Marker to indicate that flash contains data.
+            Self::DATA_MAGIC,
             self.pid.kp.to_bits(),
             self.pid.ki.to_bits(),
             self.pid.kd.to_bits(),
             (self.control_ticks >> 32) as u32,
             (self.control_ticks & 0xffffffff) as u32,
         ];
+
         flash.flash_program(base, values.as_ptr(), values.len() as isize);
     }
 }
