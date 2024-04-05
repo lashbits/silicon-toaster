de375f34f1bdac3a9e0663190fec0ad6e69f3359
Alexandru Geana <ab@geana.io>
2024-04-05 14:11:36 +0200

    fix plot-calibration.py

--- plot-calibration.py
+++ plot-calibration.py
@@ -1,49 +1,63 @@
 #!/usr/bin/python3
 
 import matplotlib.pyplot as plt
-import quicklog
 import numpy as np
+import re
 
 x = []
 y = []
 
-plt.figure()
-for record in quicklog.read_log("calibration-800.log"):
-    x.append(float(record["value"]))
-    y.append(float(record["voltage"]))
+with open("calibration-800.log", "r") as f:
+    calibration_data = f.read()
+for line in calibration_data.split('\n'):
+    result = re.match(
+        r"{\"value\"\: (?P<value>.*), \"voltage\"\: (?P<voltage>.+)}", line)
+    if result:
+        x.append(float(result.group('value')))
+        y.append(float(result.group('voltage')))
 
+# plot the original data from the silicontoaster
+plt.figure()
 plt.plot(x, y)
 
+# fit the x to y using a 4 degree polynomial
 coefs = np.polyfit(x, y, 4)
 poly = np.poly1d(coefs)
 print("Raw->Volt", coefs)
 
+# fir the y to x using a 4 degree polynomial
 coefs_inv = np.polyfit(y, x, 4)
 poly_inv = np.poly1d(coefs_inv)
 print("Volt->Raw", coefs_inv)
 
-plt.figure()
+# test the x to y polynomial
+# generate a new x with 100 linearly spaced points
 x = np.linspace(0, max(x), 100)
+# calculate the associated y for each x point
 y = []
 for xx in x:
     v = 0
     for i, c in enumerate(coefs):
-        v += c * xx ** (len(coefs) - i - 1)
+        v += c * xx**(len(coefs) - i - 1)
     y.append(v)
 
+# plot the new data, which should be identical to the original plot
+plt.figure()
 plt.plot(x, y)
-# plt.plot(x, poly(x))
 
-
+# test the y to x polynomial
+# generate a new y with 100 linearly spaced points
 y = np.linspace(0, max(y), 100)
+# calculate the associated x for each y point
 x = []
 for yy in y:
     v = 0
     for i, c in enumerate(coefs_inv):
-        v += c * yy ** (len(coefs_inv) - i - 1)
+        v += c * yy**(len(coefs_inv) - i - 1)
     x.append(v)
 
+# plot the new data, which should be identical to the original plot
 plt.figure()
 plt.plot(x, y)
-# plt.plot(x, poly_inv(x))
+
 plt.show()
