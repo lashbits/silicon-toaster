3fdd9e1ecbae08e8c48912d4d3adf75062c4b876
Alexandru Geana <ab@geana.io>
2024-11-29 09:53:48 +0100

    python code update (moved from anomaly)

--- silicontoaster/silicontoaster.py
+++ silicontoaster/silicontoaster.py
@@ -1,77 +1,86 @@
-#!/usr/bin/python3
 import struct
+import time
 from typing import Union
+import anomaly.util.logging
+
 import serial
-import serial.tools.list_ports
 
 
 class SiliconToaster:
-    def __init__(self, dev=None, sn=None):
-        if dev is not None and sn is not None:
-            raise ValueError("dev and sn cannot be set together")
 
-        if dev is None:
-            # Try to find automatically the device
-            possible_ports = []
-            for port in serial.tools.list_ports.comports():
-                # USB description string is 'SiliconToaster'.
-                if (
-                    (port.product is not None)
-                    and (port.product == "SiliconToaster")
-                    and ((sn is None) or (port.serial_number == sn))
-                ):
-                    possible_ports.append(port)
-            if len(possible_ports) > 1:
-                raise RuntimeError(
-                    "Multiple SiliconToaster devices found! I don't know which one to use."
-                )
-            elif len(possible_ports) == 1:
-                dev = possible_ports[0].device
-            else:
-                raise RuntimeError("No SiliconToaster device found")
+    # these are coefficients calculated using the calibration scripts
+    CALIBRATION_RAW_TO_V = [
+        -4.02294398e-11,
+        1.53492378e-07,
+        -2.71166328e-04,
+        7.66927146e-01,
+        -1.12729564e00,
+    ]
+    CALIBRATION_V_TO_RAW = [
+        5.59972560e-10,
+        -1.02408301e-06,
+        1.06453179e-03,
+        1.24457162e00,
+        2.57379247e00,
+    ]
 
-        self.ser = serial.Serial(dev, 9600)
-        self.calibration_raw_to_v = [
-            -4.02294398e-11,
-            1.53492378e-07,
-            -2.71166328e-04,
-            7.66927146e-01,
-            -1.12729564e00,
-        ]
-        self.calibration_v_to_raw = [
-            5.59972560e-10,
-            -1.02408301e-06,
-            1.06453179e-03,
-            1.24457162e00,
-            2.57379247e00,
-        ]
-        self._software_limit = None
+    def __init__(self, dev):
+        self._logger = anomaly.util.logging.new_logger(__name__)
+
+        self.ser = serial.Serial(dev, baudrate=9600, timeout=1)
         self.set_adc_control_on_off(True)
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.ser.close()
+
+    def close(self):
+        self.ser.close()
+
+    def _ser_write(self, data):
+        self.ser.write(data)
+        time.sleep(0.1)
+
+    def _ser_read(self, size=1):
+        return self.ser.read(size)
+
+    def _verify_response(self, expected: [bytes, bytearray]):
+        received = self._ser_read(len(expected))
+        if received != expected:
+            if len(received) == 0:
+                self._logger.error(f"expected {expected.hex()} but received no response")
+            else:
+                self._logger.error(f"expected {expected.hex()} but received {received.hex()}")
+            raise RuntimeError("received data is not what was expected")
+
     @staticmethod
-    def convert(value: Union[float, int], calibration: list[float]) -> float:
-        """Converts a value to another according to the calibration coefficients"""
+    def _convert(value: Union[float, int], calibration: list[float]) -> float:
+        """
+        Converts a value to another according to the calibration coefficients.
+        """
         v = 0.0
         value = float(value)
         for i, c in enumerate(calibration):
-            v += c * value ** (len(calibration) - i - 1)
+            v += c * value**(len(calibration) - i - 1)
         return v
 
-    def to_raw(self, value: float) -> int:
-        return int(round(self.convert(value, self.calibration_v_to_raw)))
+    def _to_raw(self, value: float) -> int:
+        return int(round(SiliconToaster._convert(value, SiliconToaster.CALIBRATION_V_TO_RAW)))
 
-    def to_volt(self, value: int) -> float:
-        return self.convert(value, self.calibration_raw_to_v)
+    def _to_volt(self, value: int) -> float:
+        return SiliconToaster._convert(value, SiliconToaster.CALIBRATION_RAW_TO_V)
 
-    def read_voltage_raw(self) -> int:
+    def _read_voltage_raw(self) -> int:
         """
         Retrieve raw ADC voltage measurement from the device.
         :return: ADC measurement.
         :rtype: int
         """
-        self.ser.write(b"\x02")
-        assert self.ser.read(1) == b"\x02"
-        return int.from_bytes(self.ser.read(2), "big", signed=False)
+        self._ser_write(b"\x02")
+        self._verify_response(b"\x02")
+        return int.from_bytes(self._ser_read(2), "big", signed=False)
 
     def read_voltage(self) -> float:
         """
@@ -79,8 +88,8 @@ class SiliconToaster:
         :return: Voltage measurement.
         :rtype: float
         """
-        raw = self.read_voltage_raw()
-        v = self.to_volt(raw)
+        raw = self._read_voltage_raw()
+        v = self._to_volt(raw)
         return v
 
     def on_off(self, enable: bool):
@@ -90,8 +99,8 @@ class SiliconToaster:
         :param enable: True or False to enable or disable the high-voltage generation.
         """
         command = b"\x01" + (b"\1" if enable else b"\0")
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x01"
+        self._ser_write(command)
+        self._verify_response(b"\x01")
 
     def set_pwm_settings(self, period: int, width: int):
         """
@@ -107,27 +116,23 @@ class SiliconToaster:
         if width < 0:
             raise ValueError("Invalid PWM width: it must be positive")
         if width >= period:
-            raise ValueError(
-                "Invalid PWM settings values: width must be lesser than period"
-            )
+            raise ValueError("Invalid PWM settings values: width must be lesser than period")
         command = bytearray(b"\x03")
         command += period.to_bytes(2, "big", signed=False)
         command += width.to_bytes(2, "big", signed=False)
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x03"
+        self._ser_write(command)
+        self._verify_response(b"\x03")
 
     def software_shoot(self, duration: int):
         """
         Generate a pulse with the device to discharge de capacitors.
         """
         if duration not in range(0x10000):
-            raise ValueError(
-                f"Invalid software shoot duration: it must be lesser than {0x10000}"
-            )
+            raise ValueError(f"Invalid software shoot duration: it must be lesser than {0x10000}")
         command = bytearray(b"\x04")
         command += duration.to_bytes(2, "big", signed=False)
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x04"
+        self._ser_write(command)
+        self._verify_response(b"\x04")
 
     def get_ticks(self) -> int:
         """
@@ -135,9 +140,9 @@ class SiliconToaster:
 
         :return: The timestamp value
         """
-        self.ser.write(b"\x05")
-        assert self.ser.read(1) == b"\x05"
-        return int.from_bytes(self.ser.read(8), "big")
+        self._ser_write(b"\x05")
+        self._verify_response(b"\x05")
+        return int.from_bytes(self._ser_read(8), "big")
 
     def get_voltage_setpoint(self) -> float:
         """
@@ -145,10 +150,10 @@ class SiliconToaster:
 
         :return: The configured voltage value to aim through the ADC Control.
         """
-        self.ser.write(b"\x06")
-        assert self.ser.read(1) == b"\x06"
-        destination = struct.unpack(">H", self.ser.read(2))[0]
-        return self.to_volt(destination)
+        self._ser_write(b"\x06")
+        self._verify_response(b"\x06")
+        destination = struct.unpack(">H", self._ser_read(2))[0]
+        return self._to_volt(destination)
 
     def set_voltage_setpoint(self, destination: float):
         """
@@ -156,10 +161,12 @@ class SiliconToaster:
 
         :param destination: The desired voltage.
         """
+        assert 0 <= destination <= 1000
+
         command = b"\x07"
-        command += struct.pack(">H", self.to_raw(destination))
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x07"
+        command += struct.pack(">H", self._to_raw(destination))
+        self._ser_write(command)
+        self._verify_response(b"\x07")
 
     def get_pwm_settings(self) -> tuple[int, int]:
         """
@@ -167,10 +174,10 @@ class SiliconToaster:
 
         :return: A tuple containing the period and the width.
         """
-        self.ser.write(b"\x08")
-        assert self.ser.read(1) == b"\x08"
-        period = int.from_bytes(self.ser.read(2), "big", signed=False)
-        width = int.from_bytes(self.ser.read(2), "big", signed=False)
+        self._ser_write(b"\x08")
+        self._verify_response(b"\x08")
+        period = int.from_bytes(self._ser_read(2), "big", signed=False)
+        width = int.from_bytes(self._ser_read(2), "big", signed=False)
         return period, width
 
     def get_adc_control_pid(self, from_flash=False):
@@ -181,10 +188,12 @@ class SiliconToaster:
         """
         command = b"\x0A"
         command += struct.pack(">?", from_flash)
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x0A"
-        return struct.unpack(">3fQ", self.ser.read(3 * 4 + 8))
+        self._ser_write(command)
+        self._verify_response(b"\x0A")
+        return struct.unpack(">3fQ", self._ser_read(3 * 4 + 8))
 
+    # TODO fix pylint
+    #pylint: disable=too-many-arguments, too-many-positional-arguments
     def set_adc_control_pid(
         self,
         kp: float,
@@ -195,8 +204,8 @@ class SiliconToaster:
     ):
         command = b"\x0B"
         command += struct.pack(">?3fQ", to_flash, kp, ki, kd, control_ticks)
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x0B"
+        self._ser_write(command)
+        self._verify_response(b"\x0B")
 
     def get_adc_control_pid_ex(self):
         """
@@ -209,13 +218,11 @@ class SiliconToaster:
             in ticks) of last PID sampling.
         """
         command = b"\x0D"
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x0D"
-        return struct.unpack(">5fQ", self.ser.read(5 * 4 + 8))
+        self._ser_write(command)
+        self._verify_response(b"\x0D")
+        return struct.unpack(">5fQ", self._ser_read(5 * 4 + 8))
 
-    def set_adc_control_pid_ex(
-        self, p_limit: float, i_limit: float, d_limit: float, output_limit: float
-    ):
+    def set_adc_control_pid_ex(self, p_limit: float, i_limit: float, d_limit: float, output_limit: float):
         """
         Set supplementary values of configuration of the ADC Control.
         Those values are transient and are reset to their default values on startup.
@@ -226,8 +233,8 @@ class SiliconToaster:
         """
         command = b"\x0C"
         command += struct.pack(">4f", p_limit, i_limit, d_limit, output_limit)
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x0C"
+        self._ser_write(command)
+        self._verify_response(b"\x0C")
 
     def set_adc_control_on_off(self, enable: bool):
         """
@@ -235,8 +242,8 @@ class SiliconToaster:
         :param enable: True or False to enable or disable the of the ADC Control.
         """
         command = b"\xAA" + (b"\1" if enable else b"\0")
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\xAA"
+        self._ser_write(command)
+        self._verify_response(b"\xAA")
 
     def adc_control_on_off(self) -> bool:
         """
@@ -245,28 +252,28 @@ class SiliconToaster:
         :return: True or False to enable or disable the of the ADC Control.
         """
         command = b"\xAB"
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\xAB"
-        return self.ser.read(1) == b"\x01"
+        self._ser_write(command)
+        self._verify_response(b"\xAB")
+        return self._ser_read(1) == b"\x01"
 
     def panic(self):
         """
         Send an illegal command in order to enter in the panic()
         """
         command = b"\x77"
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\x77"
+        self._ser_write(command)
+        self._verify_response(b"\x77")
 
     def adc_results(self) -> int:
         """
         Get values of ADC values passed to PID.
         """
         command = b"\xAC"
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\xAC"
-        v = struct.unpack(">I", self.ser.read(4))[0]
+        self._ser_write(command)
+        self._verify_response(b"\xAC")
+        v = struct.unpack(">I", self._ser_read(4))[0]
         print(v)
-        return struct.unpack(f">{v}H", self.ser.read(v * 2))
+        return struct.unpack(f">{v}H", self._ser_read(v * 2))
 
     def get_last_error(self) -> int:
         """
@@ -275,9 +282,9 @@ class SiliconToaster:
         :return: Error counter value.
         """
         command = b"\xEE"
-        self.ser.write(command)
-        assert self.ser.read(1) == b"\xEE"
-        return int.from_bytes(self.ser.read(2), "big")
+        self._ser_write(command)
+        self._verify_response(b"\xEE")
+        return int.from_bytes(self._ser_read(2), "big")
 
     def __del__(self):
         self.on_off(False)
